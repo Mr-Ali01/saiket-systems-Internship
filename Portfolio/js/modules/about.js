3143883088fb6a7d063cc9343bc746bebe11251d
Samir Ali <alisamir8565@gmail.com>
2026-08-21 18:27:19 +0530

    Commit Update Project

--- Portfolio/js/modules/about.js
+++ Portfolio/js/modules/about.js
@@ -62,3 +62,61 @@ export function renderAbout(aboutData) {
     lucide.createIcons();
   }
 }
+
+
+
+export function renderStatsCounter(statsData) {
+  const countUpData = document.getElementById("countUp-section");
+  if (!countUpData || !statsData) {
+    return;
+  }
+  countUpData.innerHTML = `
+  <div
+        class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6 sm:gap-8 divide-y sm:divide-y-0 lg:divide-x divide-(theme-border)">
+
+        ${statsData.map(stat => `
+          <div class="flex flex-col items-center text-center pt-4 sm:pt-0">
+          <div class="w-12 h-12 rounded-full theme-icon-box flex items-center justify-center mb-3">
+            <i class="fa-solid fa-code text-base text-[var(--primary)]"></i>
+          </div>
+          <span class="text-3xl sm:text-4xl font-extrabold theme-text-primary tracking-tight" data-target="${stat.value}"
+            data-suffix="${stat.suffix}">${stat.value}</span>
+          <span class="text-xs font-medium theme-text-secondary mt-1">${stat.label}</span>
+        </div>
+        `).join('')}
+       
+      </div>
+  `;
+  //  Counter Animation via IntersectionObserver
+  const statsCounter = document.querySelectorAll('[data-target]');
+  statsCounter.forEach((counter) => {
+    const target = Number(counter.dataset.target);
+    const suffix = counter.dataset.suffix;
+    const duration = 2000;
+    const startTime = performance.now();
+    function updateCounter(currentTime) {
+      const elapsedTime = currentTime - startTime;
+
+      let progress = elapsedTime / duration;
+
+      // Prevent progress from going above 1
+      progress = Math.min(progress, 1);
+      // Calculate current number
+      const currentValue = Math.floor(target * progress);
+      // Update HTML
+      counter.textContent = `${currentValue}${suffix}`;
+      // Continue animation
+      if (progress < 1) {
+
+        requestAnimationFrame(updateCounter);
+
+      } else {
+
+        // Make sure final value is exact
+        counter.textContent = `${target}${suffix}`;
+
+      }
+    }
+    requestAnimationFrame(updateCounter);
+  })
+}
